6ebf7ef557e88576f2c782596d64aaaf84c001da
jovanHillion <jovan.hillion@epitech.eu>
2025-10-30 01:51:15 +0100

    fix(global singleType): adjust some names

--- src/api/global/controllers/global.ts
+++ src/api/global/controllers/global.ts
@@ -4,4 +4,36 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::global.global');
+export default factories.createCoreController('api::global.global', ({ strapi }) => ({
+    async find(ctx) {
+        const entity = await strapi.db.query('api::global.global').findOne({
+            populate: {
+                navbar: {
+                    populate : {
+                        logo: {
+                            populate: {
+                                image: true
+                            }
+                        },
+                        dropdowns: {
+                            populate: {
+                                items: true
+                            }
+                        },
+                        linkButtons: true
+                    }
+                },
+                footer: {
+                    populate : {
+                        iconsLink: true,
+                        menu: true,
+                        legal: true
+                    }
+                },
+                favicon: true
+            }
+        });
+
+        return { data: entity };
+    }
+}));
