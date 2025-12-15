4022a68235d1c8bb748896eb032c7278647bbbce
PharaEthan <ethan1.hernou@epitech.eu>
2025-12-15 13:50:31 +0100

    feat(Quotation): update and add new document type for quotation

---
+++ src/api/quotation-game/routes/quotation-game.ts
@@ -0,0 +1,7 @@
+/**
+ * quotation-game router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::quotation-game.quotation-game');
---
+++ src/api/quotation-game/services/quotation-game.ts
@@ -0,0 +1,7 @@
+/**
+ * quotation-game service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::quotation-game.quotation-game');
